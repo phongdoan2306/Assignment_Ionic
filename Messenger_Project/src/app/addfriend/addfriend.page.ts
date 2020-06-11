272370074e2d1143e643b1cd20e84a488065fd36
khanhvy2000 <khanhvy121100@gmail.com>
2020-06-11 15:46:31 +0700

    Update Page Friends ,  AddFriends
    Thiết Kế giao diện và thực hiện các xử lý tương tác với service cho các page :  Friends , Addfriends

--- Messenger_Project/src/app/addfriend/addfriend.page.ts
+++ Messenger_Project/src/app/addfriend/addfriend.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ConnReq } from '../services/user.model';
+import { Router } from '@angular/router';
+import { ToastController, AlertController } from '@ionic/angular';
+import { UserService } from '../services/user.service';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-addfriend',
@@ -6,10 +11,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addfriend.page.scss'],
 })
 export class AddfriendPage implements OnInit {
+  listUser = [];
+  temparr = [];
+  newReq = {} as ConnReq;
+  constructor(private router: Router, private userService: UserService, private alertCtrl: AlertController,
+    private toastCtrl: ToastController) { 
+      this.userService.getNotFriends().then((resp: any) => {
+        this.listUser = resp;
+      })
+      this.userService.getListUser().then((res: any) => {
+        this.temparr = res;
+      })
 
-  constructor() { }
+    }
 
   ngOnInit() {
   }
 
+  searchuser(searchbar) {
+    this.listUser = this.temparr;
+    var q = searchbar.target.value;
+    if (q.trim() == '') {
+      return;
+    }
+    this.listUser = this.listUser.filter((v) => {
+      if (v.displayName.toLowerCase().indexOf(q.toLowerCase()) > -1) {
+        return true;
+      }
+      return false;
+    })
+  }
+
+  async sendreq(recipient) {
+    this.newReq.sender = firebase.auth().currentUser.uid;
+    this.newReq.recipient = recipient.uid;
+    if (this.newReq.sender === this.newReq.recipient) {
+      let alert = await this.toastCtrl.create({
+        message: 'This is your Nickname!',
+        duration: 2000
+      });
+      await alert.present();
+    } else {
+      let successalert = await this.alertCtrl.create({
+        header: 'Request sent',
+        message: 'Your request was sent to ' + recipient.displayName,
+        buttons: ['OK']
+      });
+      await successalert.present();
+      this.userService.sendRequest(this.newReq).then((res: any) => {
+        if (res.success) {
+          let sentuser = this.listUser.indexOf(recipient);
+          this.listUser.splice(sentuser, 1);
+        }
+      }).catch((err) => {
+        console.log(err);
+      })
+    }
+  }
+
+  backListFriends() {
+    this.router.navigate(['/friends'])
+  }
+
 }
+
--- Messenger_Project/src/app/friends/friends.page.ts
+++ Messenger_Project/src/app/friends/friends.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-friends',
@@ -6,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./friends.page.scss'],
 })
 export class FriendsPage implements OnInit {
-
-  constructor() { }
+  userData: any;
+  myFriends = [];
+  temparr = [];
+  constructor(private userService: UserService, private router: Router) {
+    this.userData = JSON.parse(localStorage.getItem('user'));
+    this.userService.getMyFriends().then((res: any) => {
+      this.myFriends = res;
+      this.temparr = res;
+    });
+   }
 
   ngOnInit() {
   }
 
+  searchuser(searchbar) {
+    this.myFriends = this.temparr;
+    var q = searchbar.target.value;
+    if (q.trim() == '') {
+      return;
+    }
+    this.myFriends = this.myFriends.filter((v) => {
+      if (v.displayName.toLowerCase().indexOf(q.toLowerCase()) > -1) {
+        return true;
+      }
+      return false;
+    })
+  }
+
+  buddyChat(buddy) {
+    this.userService.initializeBuddy(buddy);
+    this.router.navigate(['/buddychat'])
+  }
+
+  goUserInfor() {
+    this.router.navigate(['/user'])
+  }
+
+  goAddFriends() {
+    this.router.navigate(['/addfriend'])
+  }
+
 }
+

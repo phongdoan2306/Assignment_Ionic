5a0e06a4830c39259ace9c603b3c204e45effd7d
Holong1804 <58680593+Holong1804@users.noreply.github.com>
2020-06-11 15:15:35 +0700

    update page detail
    Thiết kế giao diện và thực hiện các xử lý tương tác với service cho page: detail

--- Messenger_Project/src/app/detail/detail.page.ts
+++ Messenger_Project/src/app/detail/detail.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +10,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailPage implements OnInit {
 
-  constructor() { }
+  displayName: any;
+  userData: any;
+  photoUrl: any;
 
-  ngOnInit() {
+  constructor(private router: Router, private userService: UserService, private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController, private zone: NgZone
+  ) {
+    this.userData = JSON.parse(localStorage.getItem('user'));
+    this.displayName = this.userData.displayName;
+    this.photoUrl = this.userData.photoURL;
   }
 
+  ngOnInit() {
+
+  }
+
+  async editImage(files: FileList) {
+    // let load = await this.loadingCtrl.create({
+    //   message: 'Please wait...',
+    //   translucent: false
+    // });
+    // await load.present();
+    this.photoUrl = files.item(0);
+    let updateImage = await this.userService.updateImage(this.photoUrl).then((snap: any) => {
+      // this.userService.deleteImage(this.photoUrl).then((res: any) => {
+      // if (snap.success) {
+      console.log(snap)
+      // this.zone.run(() => {
+      this.photoUrl = snap;
+      this.userService.setUserOnLocal();
+      // })
+      // load.dismiss();
+      // }
+      // }).catch((err) => {
+      //   console.log(err.message);
+      // })
+    }).catch((err) => {
+      console.log(err.message);
+    })
+
+  }
+
+  async editname() {
+    let alert = await this.alertCtrl.create({
+      header: 'Edit Nickname',
+      inputs: [{
+        name: 'nickname',
+        placeholder: 'Nickname'
+      }],
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel',
+        handler: data => {
+        }
+      },
+      {
+        text: 'Edit',
+        handler: data => {
+          if (data.nickname) {
+            this.userService.updateDisplayName(data.nickname).then(async (res: any) => {
+              if (res.success) {
+                let toast = await this.loadingCtrl.create({
+                  message: 'Your settings have been saved.',
+                  duration: 2000
+                });
+                await toast.present();
+                this.zone.run(() => {
+                  this.displayName = data.nickname;
+                  this.userService.setUserOnLocal();
+                });
+              }
+              else {
+                let toast = await this.loadingCtrl.create({
+                  message: 'Your update failed.',
+                  duration: 2000
+                });
+                await toast.present();
+              }
+            })
+          }
+        }
+      }]
+    });
+    alert.present();
+  }
+
+  backSetting() {
+    this.router.navigate(["/user"])
+  }
 }
